3900f291d3d5a76009c21e364b7f5d9f5debb7b4
shubhamsks <shubhamkumar7051@gmail.com>
2020-02-16 18:24:10 +0530

    simple node example

---
+++ index.js
@@ -0,0 +1,18 @@
+const rect = {
+    perimeter :(x, y)=>(2*(x+ y)),
+    area :(x,y)=>(x*y),
+};
+const solveRectangle=(l,b)=>{
+    console.log(`Solving for rectangle with l=${l} and b=${b}`);
+    if(l <= 0 || b <= 0) {
+        console.error("Rectangle dim should be greater than 0\n");
+    } else{
+        console.log(`Area of the rectangle is ${rect.area(l,b)}`);
+        console.log(`Perimeter of the rectangle is ${rect.perimeter(l,b)}\n`);
+    }
+}
+
+solveRectangle(2,4);
+solveRectangle(3,5);
+solveRectangle(9,0);
+solveRectangle(-3,4);
